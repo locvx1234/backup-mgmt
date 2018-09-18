62a6d002def66e01316401e32939ca3d15379b5f
Loc VU <locvx1234@gmail.com>
2018-09-18 23:00:34 +0700

    static

--- backup/backup/settings.py
+++ backup/backup/settings.py
@@ -124,8 +124,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-STATIC_ROOT =  os.path.join(BASE_DIR, 'app/static')
+#STATIC_ROOT =  os.path.join(BASE_DIR, 'app/static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    ('app', os.path.join(BASE_DIR, 'app', 'static')),
+]
+
+
 LOGIN_REDIRECT_URL = 'index'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
