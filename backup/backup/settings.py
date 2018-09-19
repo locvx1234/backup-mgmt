58fb9c99d9dde58657d4857dd1f0ec4226e69df1
Loc VU <locvx1234@gmail.com>
2018-09-19 09:22:19 +0700

    fix static

--- backup/backup/settings.py
+++ backup/backup/settings.py
@@ -127,7 +127,6 @@ USE_TZ = True
 #STATIC_ROOT =  os.path.join(BASE_DIR, 'app/static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
     ('app', os.path.join(BASE_DIR, 'app', 'static')),
 ]
 
